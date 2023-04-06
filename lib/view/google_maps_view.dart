cafbb0a182a645c5ddf3399775d2fe1c25a84ba1
Atakan Arslan <atak.arslan@hotmail.com>
2023-04-07 00:20:24 +0300

    MyOrchard

--- lib/view/google_maps_view.dart
+++ lib/view/google_maps_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -9,29 +10,45 @@ class GoogleMapsView extends StatefulWidget {
 }
 
 class _GoogleMapsViewState extends State<GoogleMapsView> {
+  late GoogleMapController controller;
   static final CameraPosition _bahcem = CameraPosition(
     target: LatLng(39.6620261, 32.8609159),
     zoom: 19.151926040649414,
   );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: _bahcem,
-        onMapCreated: (map) {
-        },
-        markers: _createMarker()
+          mapType: MapType.normal,
+          initialCameraPosition: _bahcem,
+          onMapCreated: (map) async {
+            controller = map;
+            await _createMarkerImageFromAsset(context);
+            setState(() {});
+          },
+          markers: _createMarker(),
       ),
-      );
+    );
   }
-  Set<Marker> _createMarker(){
-    return <Marker>{
+
+    BitmapDescriptor mapIcon;
+
+  Future<void> _createMarkerImageFromAsset(BuildContext context) async {
+    final ImageConfiguration imageConfiguration = createLocalImageConfiguration(
+        context);
+    var bitmap = await BitmapDescriptor.fromAssetImage(
+        imageConfiguration, 'assets/images/mapicon.png');
+    mapIcon = bitmap;
+    setState(() {});
+  }
+
+  Set<Marker> _createMarker() {
+    return <Marker>[
       Marker(markerId: MarkerId("asda"),
-      position: _bahcem.target,
-        icon: BitmapDescriptor.fromAssetImage(configuration, assetName)
+        position: _bahcem.target,
+        icon: mapIcon,
       )
-    };
+    ].toSet();
   }
 }
-
