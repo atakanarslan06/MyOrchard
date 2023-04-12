b74098f699011b7fa6f1dde450ae1bc6cf7eb4ab
Atakan Arslan <82101800+atakanarslan06@users.noreply.github.com>
2023-04-12 22:56:05 +0300

    MyOrchard

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:meyvebahcem/view/screens/onboarding.dart';
+import 'package:meyvebahcem/view/screens/AcilisEkran.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.lightGreen,
       ),
-      home: const Onboarding(),
+      home: const AcilisEkran(),
     );
   }
 }
--- lib/model/content_model.dart
+++ lib/model/content_model.dart
@@ -1,24 +1,24 @@
-class UnboardingContent {
-  String image;
-  String title;
-  String description;
+class AcilisEkranContent {
+  String resim;
+  String baslik;
+  String bilgi;
 
-  UnboardingContent({required this.image,  required this.title,  required this.description});
+  AcilisEkranContent({required this.resim,  required this.baslik,  required this.bilgi});
 }
-List<UnboardingContent> contents = [
-  UnboardingContent(
-    title: 'Verimli Bahçe',
-    image: 'assets/images/img1.jpg',
-    description: "Bahçe yönetiminde sana yardımcı olacak, ektiğin bitkilerin kontrolünü sağlayacak."
+List<AcilisEkranContent> contents = [
+  AcilisEkranContent(
+    baslik: 'Verimli Bahçe',
+    resim: 'assets/images/img1.jpg',
+    bilgi: "Bahçe yönetiminde sana yardımcı olacak, ektiğin bitkilerin kontrolünü sağlayacak."
   ),
-  UnboardingContent(
-    title: 'Bitki Dünyası',
-    image: 'assets/images/img2.jpg',
-    description: "Bitkilerin dünyasına daha yakından bakacak ve daha verimli bitki yetiştirebileceksin."
+  AcilisEkranContent(
+    baslik: 'Bitki Dünyası',
+    resim: 'assets/images/img2.jpg',
+    bilgi: "Bitkilerin dünyasına daha yakından bakacak ve daha verimli bitki yetiştirebileceksin."
   ),
-  UnboardingContent(
-    title: 'Bilinçli Tarım',
-    image: 'assets/images/img3.jpg',
-    description: "Onlarca bitki hakkında detaylı bilgi edinerek toprağına ne ekeceğin artık daha kolay. Hemen aramıza katıl."
+  AcilisEkranContent(
+    baslik: 'Bilinçli Tarım',
+    resim: 'assets/images/img3.jpg',
+    bilgi: "Onlarca bitki hakkında detaylı bilgi edinerek toprağına ne ekeceğin artık daha kolay. Hemen aramıza katıl."
   ),
 ];
